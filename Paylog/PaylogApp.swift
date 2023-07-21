6d8a7ffe65f142f1c817ac4e4088966300618bf4
Abdulrahim Iliasu Illo <abdulrahim.illo@warnermedia.com>
2023-07-21 21:17:53 +0200

    Initial Commit

---
+++ Paylog/PaylogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaylogApp.swift
+//  Paylog
+//
+//  Created by Abdulrahim Illo on 2023. 07. 21..
+//
+
+import SwiftUI
+
+@main
+struct PaylogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
